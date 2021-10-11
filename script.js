78264f156f321d6edceb955463791e75871d6c90
amal371d <amal371d@stud.kea.dk>
2021-10-11 10:40:41 +0200

    a

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ gsap.registerPlugin(ScrollTrigger);
 const section1 = document.querySelector("#section1");
 
 const billede = document.querySelector(".image1");
-const tekst = document.querySelector(".tekst1");
+const tekst1 = document.querySelector(".tekst1");
 
 gsap
   .timeline({
     scrollTrigger: {
       trigger: section1,
-      start: "-20% 60%",
+      start: "35% 60%",
       end: "+=200",
       scrub: true,
       markers: true,
@@ -18,7 +18,7 @@ gsap
   .from(billede, {
     opacity: 0,
   })
-  .from(tekst, {
+  .from(tekst1, {
     opacity: 0,
     y: 100,
     stagger: 0.1,
@@ -28,21 +28,23 @@ gsap
 const section2 = document.querySelector("#section2");
 
 const flaske = document.querySelector(".image_flaske");
+const tekst2 = document.querySelector(".tekst2");
 
 gsap
   .timeline({
     scrollTrigger: {
-      trigger: section1,
-      start: "-20% 60%",
+      trigger: section2,
+      start: "20% 60%",
       end: "+=200",
       scrub: true,
       markers: true,
     },
   })
-  .from(billede, {
+  .from(flaske, {
     opacity: 0,
+    x: 100,
   })
-  .from(tekst, {
+  .from(tekst2, {
     opacity: 0,
     y: 100,
     stagger: 0.1,
